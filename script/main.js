25bf71abade0baa29200fef7ea3a56cfe0c38fce
danylo <z1keldanulo281@gmail.com>
2023-08-22 19:16:38 +0300

    5

--- script/main.js
+++ script/main.js
@@ -2,15 +2,26 @@ let summon = document.querySelector('.star');
 let imgCont = document.querySelector('.img-cont');
 let zra4ok1 = document.querySelector('.zra4ok1');
 let zra4ok2 = document.querySelector('.zra4ok2');
+let b1 = document.querySelector('.b1');
+let b2 = document.querySelector('.b2');
+let b3 = document.querySelector('.b3');
 
-document.addEventListener('mousemove', (ev) => {
-    let x = ev.clientX * 100 / window.innerWidth + "%";
-    let y = ev.clientY * 100 / window.innerHeight + "%";
-
+b1.addEventListener('mousemove', (ev) => {
    
+    zra4ok1.classList.toggle('zra4ok1-b1');
+    zra4ok2.classList.toggle('zra4ok2-b1');
+})
 
-    console.log(ev.x, ev.y);
+b2.addEventListener('mousemove', (ev) => {
    
+    zra4ok1.classList.toggle('zra4ok1-b2');
+    zra4ok2.classList.toggle('zra4ok2-b2');
+})
+
+b3.addEventListener('mousemove', (ev) => {
+   
+    zra4ok1.classList.toggle('zra4ok1-b3');
+    zra4ok2.classList.toggle('zra4ok2-b3');
 })
 
 summon.addEventListener('click', (ev) => {
